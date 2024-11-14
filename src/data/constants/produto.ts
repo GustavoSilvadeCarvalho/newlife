68da1ca02acfe608055ea03821cf0c99b671db6f
GUSTAVO SILVA DE CARVALHO <1051392411038@fatec>
2024-11-14 10:41:18 -0300

    adicionando os primeiros arquivos do projeto

---
+++ src/data/constants/produto.ts
@@ -0,0 +1,5 @@
+import Produto from "../model/Produto";
+
+const produtos: Produto[] = [
+    
+]
---
+++ src/data/model/ItemCarrinho.ts
@@ -0,0 +1,6 @@
+import Produto from "./Produto";
+
+export default interface ItemCarinho {
+    produto: Produto
+    quantidade: number
+}
